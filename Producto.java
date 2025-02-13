3aea5a01cc4adfb7ced32629e5fc076cc9f55339
Luis <luisvj.dev@gmail.com>
2025-02-13 12:06:14 -0500

    Guia1-first commit

---
+++ Producto.java
@@ -0,0 +1,17 @@
+public class Producto {
+
+	private int codigo;
+
+	private int nombreProducto;
+
+	private int precioProducto;
+
+	private int cantidad;
+
+	private TipoProducto (enum) tipo;
+
+	public void mostrarInfo() {
+
+	}
+
+}
---
+++ Tienda.java
@@ -0,0 +1,27 @@
+public class Tienda {
+
+	private string nombreTienda;
+
+	private int dirección;
+
+	private int telefono;
+
+	private Producto[] producto;
+
+	public void venderProducto() {
+
+	}
+
+	public void agregarProducto() {
+
+	}
+
+	public void buscarPRoducto() {
+
+	}
+
+	public void setPrecio() {
+
+	}
+
+}
